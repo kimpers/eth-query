1bf4b55c70b915f23a4c6df6e2b9d85550fd67ef
Kim Persson <kimpersson88@gmail.com>
2018-10-28 12:18:06 +0100

    ADDS basic tx fetching from etherscan and filter on transaction type

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,6 +1,33 @@
+const axios = require('axios');
+const ETHERSCAN_API_URL = 'http://api.etherscan.io/api';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+const TX_TYPE_MAPPING = {
+  normal: 'txlist',
+  internal: 'internal',
+  token: 'tokentx'
+};
+
+const transactions = async (_, { address, txType }) => {
+  const { data } = await axios.get(ETHERSCAN_API_URL, {
+    params: {
+      module: 'account',
+      action: TX_TYPE_MAPPING[txType],
+      address
+    }
+  });
+
+  if (data.message !== 'OK') {
+    console.log(data);
+    throw new Error(`Non OK response from etherscan API`);
+  }
+
+  return data.result;
+};
+
 const resolvers = {
   Query: {
-    hello: () => "Hello world!"
+    transactions
   }
 };
 
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,8 +1,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  enum TxType {
+   normal
+   internal
+   token
+  }
+
+  type Transaction {
+    blockNumber: String
+    timeStamp: String
+    hash: String
+    nonce: String
+    blockHash: String
+    transactionIndex: String
+    from: String
+    to: String
+    value: String
+    gas: String
+    gasPrice: String
+    isError: String
+    txreceipt_status: String
+    input: String
+    contractAddress: String
+    cumulativeGasUsed: String
+    gasUsed: String
+    confirmations: String
+  }
   type Query {
-    hello: String
+    transactions(address: String! txType: TxType): [Transaction!]
   }
 `;
 
